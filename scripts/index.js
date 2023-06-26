4381b5b2e2040a0cdff0af44cfc43b307c883309
Andrey Nurullin <andrey.nurullin@gmail.com>
2023-06-26 11:30:53 +0300

    feat: сделать добавление карточек c помощью JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 let popup = document.querySelector('.popup');
 let openPopupButton = document.querySelector('.profile__edit-button');
 let closePopupButton = document.querySelector('.popup__close-button');
@@ -30,6 +57,24 @@ function handleFormSubmit(e) {
   handlePopupVisibility();
 }
 
+function getCardElement(name, link) {
+  const cardTemplate = document.querySelector('#card').content;
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.cards-grid__card-photo').src = link;
+  cardElement.querySelector('.cards-grid__card-caption').textContent = name;
+  return cardElement;
+}
+
+function addCardElement(name, link) {
+  let cardNode = getCardElement(name, link);
+  document.querySelector('.cards-grid').prepend(cardNode);
+}
+
+function initCards(cardDataArray) {
+  cardDataArray.forEach(cardData => addCardElement(cardData.name, cardData.link));
+}
+
+initCards(initialCards);
 openPopupButton.addEventListener('click', initPopup);
 closePopupButton.addEventListener('click', handlePopupVisibility);
 profileForm.addEventListener('submit', handleFormSubmit);
